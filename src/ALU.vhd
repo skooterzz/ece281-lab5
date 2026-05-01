e6abd59fe2d3860ef191d2993791fd009d8148ee
SKOOTERZZ <230196682+skooterzz@users.noreply.github.com>
2026-05-01 11:20:19 -0600

    more ALU

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -52,18 +52,39 @@ end component ripple_adder;
 -- declare signals w_i_B
 signal w_i_B : std_logic_vector(7 downto 0);
 signal w_ALU_result : std_logic_vector(7 downto 0);
+signal w_Or : std_logic_vector(7 downto 0);
+signal w_And : std_logic_vector(7 downto 0);
+signal w_Add : std_logic_vector(7 downto 0);
+signal w_Sub: std_logic_vector(7 downto 0);
+signal w_Cout : std_logic;
 
 begin
 -- port maps -- 
-    Port(
-    w_i_B <= i_B;
-    
+    ripple_adder_inst: ripple_adder
+    port map(
+        A     => i_A,
+        B     => w_i_B,
+        Cin   => i_op(0),   -- Directly to input here
+        S    => w_ALU_result,
+        Cout  => w_Cout
     );
 
 
-w_i_B <= when (i_op(0) = '0') else not i_B;
+w_Or <= i_A or i_B;
+w_And <= i_A and i_B;
+w_i_B <= i_B when (i_op(0) = '0') else 
+          not i_B;
+
+w_ALU_result <= w_Add when (i_op = "000") else
+            w_Sub when (i_op = "001") else
+            w_And when (i_op = "010") else
+            w_Or when (i_op = "011");
+            
+o_result <= w_ALU_result;
 
 --flags
 o_flags(0) <= not(w_ALU_result(7) or w_ALU_result(6) or w_ALU_result(5) or w_ALU_result(4) or w_ALU_result(3) or w_ALU_result(2) or w_ALU_result(1) or w_ALU_result(0));  
 -- negative flag -> look @ MSB to see if its 1 or 0 (neg or pos)
+o_flags(3) <= w_ALU_result(7);
+
 end Behavioral;
